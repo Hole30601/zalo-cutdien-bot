60125bdbca69ba57e23679d2a2b779febcd65ab7
Hole 30601 <121752219+Hole30601@users.noreply.github.com>
2026-05-22 20:00:14 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    BOT_TOKEN:
+    process.env.BOT_TOKEN,
+    
+    URL:
+    process.env.URL_SEX,
+
+    PHOTO_URL:
+    process.env.PHOTO_URL,
+    
+    ADMIN_ID:
+    process.env.ADMIN_ID
+};
---
+++ firebase.js
@@ -0,0 +1,33 @@
+const admin = require("firebase-admin");
+
+if(!admin.apps.length){
+
+     
+     admin.initializeApp({
+ 
+      credential:
+      admin.credential.cert({
+
+         projectId:
+         process.env.FB_PROJECT_ID,
+
+         clientEmail:
+         process.env.FB_CLIENT_EMAIL,
+
+         privateKey:
+         process.env.FB_PRIVATE_KEY
+         .replace(/\\n/g, "\n")
+              
+
+      }),
+
+      databaseURL:
+      process.env.FB_DB_URL
+
+   });
+}
+
+const db =
+admin.database();
+
+module.exports = db;
